80ba24001df5e5e36ad83edb82bfa2037bf15096
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-18 12:14:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of Rectangles in a Circle/number-of-rectangles-in-a-circle.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            int ans = ob.rectanglesInCircle(N);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int rectanglesInCircle(int r) {
+        // code here
+        int ans=0;
+        for(int i=1;i<2*r;i++){
+            for(int j=1;j<2*r;j++){
+                if(i==0 || j==0) continue;
+                // we want sqrt of (x^2+y^2)<=2*r
+                
+                if(i*i+j*j<=4*r*r) ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
