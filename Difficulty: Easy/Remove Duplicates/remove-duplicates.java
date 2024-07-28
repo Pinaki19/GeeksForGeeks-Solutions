3564d8656aba6537fc45a129328c387bc0538b94
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-28 23:10:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+
+            Solution ob = new Solution();
+            String result = ob.removeDups(s);
+
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    String removeDups(String str) {
+        // code here
+        boolean[] chars=new boolean[26];
+        Arrays.fill(chars,false);
+        String ans="";
+        
+        for(int i=0;i<str.length();i++){
+            char c=str.charAt(i);
+            int t=(int)(c-'a');
+            if(chars[t]) continue;
+            chars[t]=true;
+            ans+=c;
+        }
+        
+        return ans;
+    }
+}
