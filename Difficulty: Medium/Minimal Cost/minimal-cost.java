cca62fb3d34082f8c102e2e2f7e0a8e22944eb65
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-09-28 17:00:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public int minimizeCost(int k, int arr[]) {
+        // code here
+        int n=arr.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,(int)1e5);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=1;j<=k;j++){
+                if(i+j>=n) continue;
+                dp[i]=Math.min(dp[i],Math.abs(arr[i]-arr[i+j])+dp[i+j]);
+            }
+        }
+        return dp[0];
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.minimizeCost(k, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
